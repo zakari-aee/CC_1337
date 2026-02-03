f5096a13c677d98126383cbe28d34f3f50f01d64
zakari-aee <zakariaealliouate@gmail.com>
2026-02-03 10:57:59 +0100

    ehhhhh

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -1,9 +1,13 @@
 #include <stddef.h>
 
-void *my_memset(void *ptr, int value, size_t num)
+void    *my_memset(void *ptr, int value, size_t num)
 {
-    unsigned char *p = ptr;
-    size_t i = 0;
+    unsigned char   *p;
+    size_t  i;
+
+    *p = ptr;
+    i = 0;
+    
     while(i < num)
     {
         p[i] = (unsigned char)value;
